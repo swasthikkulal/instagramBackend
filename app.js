dddd9a84690823ad3e5e66548da77e1197b23975
swasthikkulal <swasthik126@gmail.com>
2025-09-24 20:38:21 +0530

    Remove node_modules from tracking

--- app.js
+++ app.js
@@ -3,13 +3,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var dotenv = require('dotenv');
+
+dotenv.config(); // Load .env
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var usersRouter = require('./routes/users');
+
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("MongoDB connection error:", err));
 
 var app = express();
 
-// view engine setup
+// view engine setup (optional if you have no views)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -18,24 +29,28 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Optional: uploads folder (only for testing locally)
 app.use("/uploads", express.static("uploads"));
+
 app.use('/', indexRouter);
 // app.use('/users', usersRouter);
+
+// Simple ping route for Render health check
+app.get('/ping', (req, res) => res.json({ status: 'ok' }));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// error handler (send JSON instead of EJS on Render)
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    message: err.message,
+    error: req.app.get('env') === 'development' ? err : {}
+  });
 });
 
 module.exports = app;
